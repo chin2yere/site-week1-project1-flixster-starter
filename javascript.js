bad404bf51208fb3dc4f9cc13b3e9c79686d3184
Chinyere Offor <chinyereoffor@meta.com>
2023-06-16 11:02:24 -0700

    Search function implemented

--- javascript.js
+++ javascript.js
@@ -1,15 +1,20 @@
 var gifsDiv = document.getElementById("gifsDiv");
 var ldBtn = document.getElementById("ldBtn");
 var middlePart = document.getElementById("middlePart");
+var searchBtn = document.getElementById("searchBtn");
+var searchInput = document.getElementById("searchInput");
+var clearBtn = document.getElementById("clearBtn");
+var images;
+var inputValue;
 
 const apiKey = "cf9182cf58f52e0c0d3f0409fb37c8d8";
-url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=${apiKey}`
+url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=${apiKey}`;
 var j = 0;
 document.addEventListener("DOMContentLoaded", async (event) => {
 
     const response = await fetch(url);
     const data = await response.json();
-    const images = data.results;
+    images = data.results;
 
 
 
@@ -41,13 +46,75 @@ ldBtn.addEventListener("click", async (event) => {
     try {
         //const apiKey = "cf9182cf58f52e0c0d3f0409fb37c8d8";
         //url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=${apiKey}`
-        const response = await fetch(url);
-        const data = await response.json();
-        const images = data.results;
+        // const response = await fetch(url);
+        // const data = await response.json();
+        // const images = data.results;
         if (j < images.length) {
             const bigDivv = document.createElement("div");
             
             bigDivv.setAttribute("class", "row");
+
+            for (let i = j; i < (j + 4); i++) {
+                if (i<images.length) {
+
+                    const img = document.createElement("img");
+                    const h1 = document.createElement("h5");
+                    const h4 = document.createElement("h6");
+                    const divv = document.createElement("div");
+                    // const bigDivv = document.createElement("gifsDiv");
+
+                    divv.setAttribute("class", "col");
+                    // bigDivv.setAttribute("class", "col");
+
+                    h4.innerHTML = images[i].vote_average;
+                    h1.innerHTML = images[i].title;
+                    img.src = `https://www.themoviedb.org/t/p/w1280/${images[i].poster_path}`;
+                    img.style.width = "220px";
+                    divv.appendChild(img);
+                    divv.appendChild(h4);
+                    divv.appendChild(h1);
+                    
+                    bigDivv.appendChild(divv);
+                    middlePart.appendChild(bigDivv);
+
+                }
+            }
+
+        }
+
+        j += 4;
+
+
+
+
+
+    } catch (error) {
+        // Handle any errors that occur during the fetch request
+        console.log(error);
+    }
+
+
+    // Perform your desired action here
+    console.log("Button clicked!");
+});
+//search button
+searchBtn.addEventListener("click", async (event) => {
+    event.preventDefault(); // Prevent form submission (optional)
+
+
+    try {
+        //const apiKey = "cf9182cf58f52e0c0d3f0409fb37c8d8";
+        inputValue= searchInput.value;
+        url = `https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&query=${encodeURIComponent(inputValue)}&api_key=${apiKey}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        images = data.results;
+        middlePart.innerHTML="";
+        
+            const bigDivv = document.createElement("div");
+            
+            bigDivv.setAttribute("class", "row");
+            j=0;
 
             for (let i = j; i < (j + 4); i++) {
 
@@ -71,7 +138,7 @@ ldBtn.addEventListener("click", async (event) => {
                 bigDivv.appendChild(divv);
                 middlePart.appendChild(bigDivv);
 
-            }
+            
         }
 
         j += 4;
@@ -89,6 +156,69 @@ ldBtn.addEventListener("click", async (event) => {
     // Perform your desired action here
     console.log("Button clicked!");
 });
+
+//clear button
+clearBtn.addEventListener("click", async (event) => {
+    event.preventDefault(); // Prevent form submission (optional)
+
+
+    try {
+        //const apiKey = "cf9182cf58f52e0c0d3f0409fb37c8d8";
+        searchInput.value="";
+        url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=${apiKey}`
+        const response = await fetch(url);
+        const data = await response.json();
+        images = data.results;
+        middlePart.innerHTML="";
+        
+            const bigDivv = document.createElement("div");
+            
+            bigDivv.setAttribute("class", "row");
+            j=0;
+
+            for (let i = j; i < (j + 4); i++) {
+
+                const img = document.createElement("img");
+                const h1 = document.createElement("h5");
+                const h4 = document.createElement("h6");
+                const divv = document.createElement("div");
+                // const bigDivv = document.createElement("gifsDiv");
+
+                divv.setAttribute("class", "col");
+                // bigDivv.setAttribute("class", "col");
+
+                h4.innerHTML = images[i].vote_average;
+                h1.innerHTML = images[i].title;
+                img.src = `https://www.themoviedb.org/t/p/w1280/${images[i].poster_path}`;
+                img.style.width = "220px";
+                divv.appendChild(img);
+                divv.appendChild(h4);
+                divv.appendChild(h1);
+                
+                bigDivv.appendChild(divv);
+                middlePart.appendChild(bigDivv);
+
+            
+        }
+
+        j += 4;
+
+
+
+
+
+    } catch (error) {
+        // Handle any errors that occur during the fetch request
+        console.log(error);
+    }
+
+
+    // Perform your desired action here
+    console.log("Button clicked!");
+});
+
+
+
 
 
 
