a0ab372aef47ed5f52cbec044492dcbacdabccfc
Chinyere Offor <chinyereoffor@meta.com>
2023-06-16 08:09:28 -0700

    default mechanism working

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 var gifsDiv = document.getElementById("gifsDiv");
 var ldBtn = document.getElementById("ldBtn");
+var middlePart = document.getElementById("middlePart");
 
 const apiKey = "cf9182cf58f52e0c0d3f0409fb37c8d8";
 url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=${apiKey}`
@@ -12,20 +13,21 @@ document.addEventListener("DOMContentLoaded", async (event) => {
 
 
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 4; i++) {
         const img = document.createElement("img");
-        const h1 = document.createElement("h4");
-        const h4 = document.createElement("h5");
+        const h1 = document.createElement("h5");
+        const h4 = document.createElement("h6");
         const divv = document.createElement("div");
         divv.setAttribute("class", "col");
 
-        h4.innerHTML = images[i].vote_count;
+        h4.innerHTML = images[i].vote_average;
         h1.innerHTML = images[i].title;
         img.src = `https://www.themoviedb.org/t/p/w1280/${images[i].poster_path}`;
-        img.style.width = "200px";
-        divv.appendChild(h1);
+        img.style.width = "220px";
         divv.appendChild(img);
         divv.appendChild(h4);
+        divv.appendChild(h1);
+        
         gifsDiv.appendChild(divv);
         j += 1;
 
@@ -47,31 +49,32 @@ ldBtn.addEventListener("click", async (event) => {
             
             bigDivv.setAttribute("class", "row");
 
-            for (let i = j; i < (j + 3); i++) {
+            for (let i = j; i < (j + 4); i++) {
 
                 const img = document.createElement("img");
-                const h1 = document.createElement("h4");
-                const h4 = document.createElement("h5");
+                const h1 = document.createElement("h5");
+                const h4 = document.createElement("h6");
                 const divv = document.createElement("div");
                 // const bigDivv = document.createElement("gifsDiv");
 
                 divv.setAttribute("class", "col");
                 // bigDivv.setAttribute("class", "col");
 
-                h4.innerHTML = images[i].vote_count;
+                h4.innerHTML = images[i].vote_average;
                 h1.innerHTML = images[i].title;
                 img.src = `https://www.themoviedb.org/t/p/w1280/${images[i].poster_path}`;
-                img.style.width = "200px";
-                divv.appendChild(h1);
+                img.style.width = "220px";
                 divv.appendChild(img);
                 divv.appendChild(h4);
+                divv.appendChild(h1);
+                
                 bigDivv.appendChild(divv);
-                document.body.appendChild(bigDivv);
+                middlePart.appendChild(bigDivv);
 
             }
         }
 
-        j += 3;
+        j += 4;
 
 
 
